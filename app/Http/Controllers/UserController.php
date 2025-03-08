63adb2204e7834142e0f068391c054073ab98d0c
Kryllee <k.laderaaa@gmail.com>
2025-03-08 19:05:20 +0800

    initial commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,15 +2,101 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\User;
-Class UserController extends Controller {
+use Illuminate\Http\Response;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+use DB;
+
+class UserController extends Controller
+{
     private $request;
+
     public function __construct(Request $request){
         $this->request = $request;
- }
+    }
+
+    /* Helper method to return a successful JSON response */
+    protected function successResponse($data, $code = Response::HTTP_OK){
+        return response()->json(['data' => $data], $code);
+    }
+
+    /* Helper method to return an error JSON response */
+    protected function errorResponse($message, $code){
+        return response()->json(['error' => $message, 'code' => $code], $code);
+    }
+
+    /* Return the list of users */
+    public function index(){
+        $users = User::all();
+        return $this->successResponse($users);
+    }
+
+    /* Get all users */
     public function getUsers(){
         $users = User::all();
-        return response()->json($users, 200);
+        return $this->successResponse($users);
+    }
+
+    /*Add a new user*/
+    public function add(Request $request){
+        $rules = [
+            'Username' => 'required|max:20',
+            'Password' => 'required|max:20',
+            'Gender' => 'required|in:Male,Female',
+        ];
+        
+        $this->validate($request, $rules);
+        $user = User::create($request->all());
+        return $this->successResponse($user, Response::HTTP_CREATED);
+    }
+
+    /*Show details of a single user*/
+    public function show($id){
+        $user = User::find($id);
+
+        if (!$user) {
+            return $this->errorResponse('User ID does not exist', Response::HTTP_NOT_FOUND);
+        }
+
+        return $this->successResponse($user);
+    }
+
+    /*Update an existing user*/
+    public function update(Request $request, $id){
+        $rules = [
+            'Username' => 'max:20',
+            'Password' => 'max:20',
+            'Gender' => 'in:Male,Female',
+        ];
+
+        $this->validate($request, $rules);
+        $user = User::find($id);
+
+        if (!$user) {
+            return $this->errorResponse('User ID does not exist', Response::HTTP_NOT_FOUND);
+        }
+
+        $user->fill($request->all());
+
+        if ($user->isClean()) {
+            return $this->errorResponse('At least one value must change', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        $user->save();
+        return $this->successResponse($user);
+    }
+
+    /* Delete a user */
+    public function delete($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return $this->errorResponse('User ID does not exist', Response::HTTP_NOT_FOUND);
+        }
+
+        $user->delete();
+        return $this->successResponse(['message' => 'User deleted successfully']);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -8,8 +8,8 @@ use Illuminate\Database\Eloquent\Model;
     class User extends Model{
         protected $table = 'tbluser';
         // column sa table
-        protected $fillable = ['username', 'password','gender'];
+        protected $fillable = ['Username', 'Password','Gender'];
 
         public $timestamps = false;
-        protected $primaryKey = 'id';
+        //protected $primaryKey = 'id';
  }
